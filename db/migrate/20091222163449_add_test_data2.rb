869098bdf88d79c8152c45fc48d356be0fcf44a8
Timothy Yen <yen.timothy@gmail.com>
2009-12-22 12:04:32 -0500

    Added css files

---
+++ db/migrate/20091222163449_add_test_data2.rb
@@ -0,0 +1,20 @@
+class AddTestData2 < ActiveRecord::Migration
+  def self.up
+  	Product.delete_all
+	Product.create(:title => 'Pragmatic Version control',
+			:description =>
+				%{<p>
+					This book is a recipe-based approach to using Subversion that will
+					get you up and runnign quickly---and correctly. All projects need
+					version control: it's a foundational piece of any project's
+					infrastructure. yet half of all project teams in the U.S. don't use
+					any version control at all. many others don't use it well, and end
+					up experiencing time-consuming problems.
+				</p>},
+			:image_url => '/images/svn/jpg',
+			:price => 28.50)
+  end
+
+  def self.down
+  end
+end
