7d63a13a2abb9be8933b16336fe52b8c7d9abdad
Timothy Yen <yen.timothy@gmail.com>
2009-12-23 11:01:22 -0500

    Completed 9.1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,16 +1,35 @@
 class StoreController < ApplicationController
   def index
-  		@products = Product.find_products_for_sale
+    @products = Product.find_products_for_sale
+    @cart = find_cart
+
+    session[:counter] = 0 if session[:counter].nil?
+    session[:counter] = session[:counter] + 1
   end
 
   def add_to_cart
-  		product = Product.find(params[:id])
-		@cart = find_cart
-		@cart.add_product(product)
+    session[:counter] = 0
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @cart.add_product(product)
+    redirect_to_index
+  rescue
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid product")
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
   end
 
   private
-  		def find_cart
-			session[:cart] ||= Cart.new
-		end
+    def find_cart
+      session[:cart] ||= Cart.new
+    end
+
+    def redirect_to_index(msg = nil)
+      flash[:notice] = msg if msg
+      redirect_to :action => 'index'
+    end
 end
